735cdd36070da22f0e6bea83a413b7ee8c210071
ASIM XHAKMA <117442412+4S1M@users.noreply.github.com>
2022-12-01 19:26:09 +0600

    Add files via upload

--- GREEN1.py
+++ GREEN1.py
@@ -152,10 +152,10 @@ def menu_apikey():
 ██   ██ ███████ ██ ██      ██ 
                                                                                                                                                                                        
     ┌───────────────────────────────────────┐
-    │ [✓] AUTHOR   : MD ASIM.             │
-    │ [✓] GITHUB   : MR-ASIM              │
-    │ [✓] WHATSAPP :  +88 01962510071       │
-    │ [✓] TOOLS    : \033[1;32m RANDOM NUMBER CLONER  \033[1;37m  │
+    │ [✓] AUTHOR   : Asim Xhakma               │
+    │ [✓] GITHUB   : ASIM-404                │
+    │ [✓] WHATSAPP :  +88 01828881765       │
+    │ [✓] TOOLS    : \033[1;32m RANDOM NUMBER CLONER\033[1;37m  │
     └───────────────────────────────────────┘""")                                           
   print("\t \033[1;32m  FIRST GET APPROVEL\033[1;37m ")
   print("")
